5dadd7532a5d576e951a404d9f5272eb6c922fc0
Kadiresh07 <kadiresh077@gmail.com>
2025-12-12 08:57:01 +0530

    added new update in  experties

--- src/app/experties.js
+++ src/app/experties.js
@@ -4,47 +4,65 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function FieldOfExpertise() {
-  const expertise = [
+  const expertise = [ 
+    {
+      src: "/ovarian.webp",
+      hoverSrc: "/ovarian-white.webp",
+      title: "Ovarian Stimulation, IVF, ICSI",
+      description:"Boosting egg production with IVF/ICSI."
+    },
+    {
+      src: "/embruo.webp",
+      hoverSrc: "/embruo-white.webp",
+      title: " Placing the developed embryo back into the uterus",
+    },
+{
+      src: "/laproscopic.webp",
+      hoverSrc: "/WHITE-2.webp",
+      title: "Minimally invasive hystero-laparoscopic surgeries",
+    },
+
+ {
+      src: "/implantation.webp",
+      hoverSrc: "/implantation-white.webp",
+      title: "Recurrent implantation failure; immunological infertility",
+    },
+
+ {
+      src: "/reprodu.webp",
+      hoverSrc: "/reprodu-white.webp",
+      title: "Recurrent implantation failure; immunological infertility",
+    },
+
     {
       src: "/highrick.webp",
       hoverSrc: "/WHITE.webp",
       title: "High-risk obstetrics; PESA/TESA andrology procedures",
     },
-    {
-      src: "/laproscopic.webp",
-      hoverSrc: "/WHITE-2.webp",
-      title: "Minimally invasive hystero-laparoscopic surgeries",
-    },
-    {
-      src: "/ovarian.webp",
-      hoverSrc: "/ovarian-white.webp",
-      title: "Ovarian Disorders, IUI, IVF, ICSI",
-    },
-    {
-      src: "/donor.webp",
-      hoverSrc: "/donor-white.webp",
-      title: "Donor & surrogacy programs; PGT/PGD, ERA",
-    },
+    
     {
       src: "/poor-ovarian.webp",
       hoverSrc: "/poor-ovarian-white.webp",
       title: "Poor ovarian reserve management",
     },
+   
     {
-      src: "/implantation.webp",
-      hoverSrc: "/implantation-white.webp",
-      title: "Recurrent implantation failure; immunological infertility",
+      src: "/donor.webp",
+      hoverSrc: "/donor-white.webp",
+      title: "Donor & surrogacy programs; PGT/PGD, ERA",
     },
+   
     {
       src: "/ultrasonography.webp",
       hoverSrc: "/ultrasonography-white.webp",
       title: "3D ultrasonography, Dopplers; recurrent pregnancy loss",
     },
     {
-      src: "/hands.webp",
-      hoverSrc: "/hands-white.webp",
+      src: "/fertitity.webp",
+      hoverSrc: "/fertility-white.webp",
       title: "Compassionate care & patient support",
     },
+   
   ];
 
   return (
@@ -57,11 +75,11 @@ export default function FieldOfExpertise() {
               "linear-gradient(100.24deg, rgba(252, 241, 236, 0.8) 20.09%, rgba(228, 243, 253, 0.6) 48.04%, rgba(252, 243, 239, 0.5) 80.99%)",
           }}
         >
-          <h3 className="text-2xl sm:text-3xl md:text-4xl xl:text-5xl font-extrabold text-[#24305a] text-center leading-tight">
+          <h3 className="text-2xl sm:text-3xl md:text-4xl xl:text-5xl font-extrabold text-[#24305a] text leading-tight">
             Field of Expertise
           </h3>
 
-          <div className="mt-8 grid grid-cols-2 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 sm:gap-6 md:gap-8 xl:gap-12">
+          <div className="mt-8 grid grid-cols-2 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 sm:gap-6 md:gap-8 xl:gap-x-5 gap-y-6">
             {expertise.map((item, i) => (
               <ExpertiseCard key={i} item={item} />
             ))}
@@ -77,7 +95,7 @@ function ExpertiseCard({ item }) {
 
   return (
     <div className="group flex flex-col items-center justify-start rounded-3xl bg-white shadow-md transition-all duration-500 hover:bg-[#ff69b4] hover:shadow-2xl hover:scale-[1.03] px-4 py-6 sm:p-6 md:p-7 lg:p-8">
-      <div className="relative w-20 h-20 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-36 lg:h-36 mb-5">
+      <div className="relative w-20 h-20 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-36 lg:h-15 mb-1">
 
         {!loaded && (
           <div className="absolute inset-0 animate-pulse rounded-2xl bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200" />
